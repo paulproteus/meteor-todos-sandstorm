adcc22d7ed1821a723eefd39090c9e7e1d778085
Asheesh Laroia <asheesh@sandstorm.io>
2016-05-02 16:52:03 -0700

    Update global-setup.sh to vagrant-spk v0.161

--- .sandstorm/global-setup.sh
+++ .sandstorm/global-setup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
+# Set options for curl. Since we only want to show errors from these curl commands, we also use
+# 'cat' to buffer the output; for more information:
+# https://github.com/sandstorm-io/vagrant-spk/issues/158
+
 CURL_OPTS="--silent --show-error"
 echo localhost > /etc/hostname
 hostname localhost
-curl $CURL_OPTS https://install.sandstorm.io/ > /host-dot-sandstorm/caches/install.sh
+
+# The following line copies stderr through stderr to cat without accidentally leaving it in the
+# output file. Be careful when changing. See: https://github.com/sandstorm-io/vagrant-spk/pull/159
+curl $CURL_OPTS https://install.sandstorm.io/ 2>&1 > /host-dot-sandstorm/caches/install.sh | cat
+
 SANDSTORM_CURRENT_VERSION=$(curl $CURL_OPTS -f "https://install.sandstorm.io/dev?from=0&type=install")
 SANDSTORM_PACKAGE="sandstorm-$SANDSTORM_CURRENT_VERSION.tar.xz"
 if [[ ! -f /host-dot-sandstorm/caches/$SANDSTORM_PACKAGE ]] ; then
-    curl $CURL_OPTS --output "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE.partial" "https://dl.sandstorm.io/$SANDSTORM_PACKAGE"
+    echo -n "Downloading Sandstorm version ${SANDSTORM_CURRENT_VERSION}..."
+    curl $CURL_OPTS --output "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE.partial" "https://dl.sandstorm.io/$SANDSTORM_PACKAGE" 2>&1 | cat
     mv "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE.partial" "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE"
+    echo "...done."
 fi
 if [ ! -e /opt/sandstorm/latest/sandstorm ] ; then
+    echo -n "Installing Sandstorm version ${SANDSTORM_CURRENT_VERSION}..."
     bash /host-dot-sandstorm/caches/install.sh -d -e "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE" >/dev/null
+    echo "...done."
 fi
 modprobe ip_tables
 # Make the vagrant user part of the sandstorm group so that commands like
